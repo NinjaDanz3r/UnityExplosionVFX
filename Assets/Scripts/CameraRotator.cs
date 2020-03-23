9f657e87077b0ebf274b3a37529cf83ae0ac5321
DANZERPC\Danzer <danielpejcinovski@hotmail.com>
2020-03-23 22:10:51 +0100

    Added a simple rotation script.

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    [SerializeField] private float speed = 0f;
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
